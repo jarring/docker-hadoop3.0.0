66bce9991a1a27b2c8faa69c35805c4b5b1c1757
jarring <hbltsl@qq.com>
2019-01-28 22:15:15 +0800

    auto set JAVA_HOME

--- docker-container/workspace/common/hadoop-3.0.0/etc/hadoop/hadoop-env.sh
+++ docker-container/workspace/common/hadoop-3.0.0/etc/hadoop/hadoop-env.sh
@@ -1,6 +1,6 @@
 
 # others ...
-export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.161-0.b14.el7_4.x86_64-debug"
+export JAVA_HOME=`find /usr/lib/jvm/ -name "java*openjdk*debug" `
 export HADOOP_HOME="/common/hadoop-3.0.0"
 export HADOOP_LOG_DIR="/workspace/logs"
 
